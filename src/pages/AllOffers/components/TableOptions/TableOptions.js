5ddaccad79275f4f41f865876ea8c171689fd6dd
velomad <sagaryc111@gmail.com>
2021-05-09 02:04:15 -0700

    offers table

--- src/pages/AllOffers/components/TableOptions/TableOptions.js
+++ src/pages/AllOffers/components/TableOptions/TableOptions.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { DropDown } from "../../../../components";
+import { DropDown, Searchbar } from "../../../../components";
 
 const TableOptions = () => {
   return (
     <div>
-      <div className="flex border items-center space-x-4 p-4">
+      <div className="flex  items-center space-x-4 py-4">
         <div>
           <DropDown def="10" options={[10, 20, 30, 40, 50]} />
+        </div>
+        <div className="w-full">
+          <Searchbar placeHolder="Search" />
+        </div>
+        <div>
+          <DropDown def="SORT" options={[10, 20, 30, 40, 50]} />
         </div>
       </div>
     </div>
